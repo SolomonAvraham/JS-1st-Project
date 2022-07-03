af9d3bf92be497d8f036b3e0d5d85a2a1d64f75b
SolomonAvraham <Selmon_a@hotmail.com>
2022-07-03 13:02:32 +0300

    update

--- JS/main.js
+++ JS/main.js
@@ -2,7 +2,9 @@ function menuList() {
   let menuListBtn = document.getElementById("listForBtnClick");
   if (menuListBtn.style.display === "none") {
     menuListBtn.style.display = "flex";
+    header_area_one.style.backgroundColor = "green";
   } else {
+    header_area_one.style.backgroundColor = "transparent";
     menuListBtn.style.display = "none";
   }
 }
