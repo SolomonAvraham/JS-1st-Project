c1ba0f7777aefc56d72e4ef2969402a1be7656cb
SolomonAvraham <Selmon_a@hotmail.com>
2022-07-03 16:33:48 +0300

    update

--- JS/main.js
+++ JS/main.js
@@ -3,6 +3,7 @@ function menuList() {
   if (menuListBtn.style.display === "none") {
     menuListBtn.style.display = "flex";
     header_area_one.style.backgroundColor = "green";
+    header_area_one.style.borderRadius = "20%";
   } else {
     header_area_one.style.backgroundColor = "transparent";
     menuListBtn.style.display = "none";
