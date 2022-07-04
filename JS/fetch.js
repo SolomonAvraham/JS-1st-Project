64b96001e19fce0e6410e6840f329c3cb56eabc4
SolomonAvraham <Selmon_a@hotmail.com>
2022-07-04 16:12:00 +0300

    update

--- JS/fetch.js
+++ JS/fetch.js
@@ -1,11 +1,5 @@
 async function cards() {
-  let brandValue = document.getElementById("card_brand");
-  let creatAtValue = document.getElementById("card_createdAt");
-  let colorValue = document.getElementById("card_color");
-  let ramValue = document.getElementById("card_ram");
-  let isAvailableValue = document.getElementById("card_isAvailable");
-  let priceValue = document.getElementById("card_price");
-  let picValue = document.getElementById("card_pic");
+  let cardTextDisplay = document.querySelector("[data-cards=container]");
   const BASIC_API =
     "https://my-json-server.typicode.com/Jeck99/fake-server/devices";
   try {
@@ -13,14 +7,16 @@ async function cards() {
       .then((res) => res.json())
       .then((res) => {
         res.forEach((item) => {
-          console.log(item.price);
-          brandValue.innerHTML = `${item.brand}`;
-          creatAtValue.innerHTML = `${item.createdAt}`;
-          colorValue.innerHTML = `${item.color}`;
-          ramValue.innerHTML = `${item.ram}`;
-          isAvailableValue.innerHTML = `${item.isAvailable}`;
-          priceValue.innerHTML = `${item.price}$`;
-          picValue.innerHTML = `<img src="${item.picture}" alt="">`;
+          cardTextDisplay.innerHTML += ` 
+          <div class="card_container" >
+          <img src="https://media.4rgos.it/i/Argos/9520608_R_Z001A?w=750&h=440&qlt=70" width="100%" alt="phone-pic">
+            </div><br><br>
+            <div class="card_text"><div>Brand: <span  >${item.brand}</span></div>
+            <div>Created At: <span >${item.createdAt}</span></div>
+            <div>Color: <span  >${item.color}</span></div>
+            <div>Ram: <span  >${item.ram}</span></div>
+            <div>is Available: <span  >${item.isAvailable}</span></div>
+            <div>Price: <span >${item.price}$</span></div></div><br><br>`;
         });
       });
   } catch (error) {
