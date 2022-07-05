51d1d5f19ea2e71b5acfd7ac4c03b2ee25efa93b
SolomonAvraham <Selmon_a@hotmail.com>
2022-07-05 19:17:53 +0300

    update

--- JS/fetch.js
+++ JS/fetch.js
@@ -14,12 +14,18 @@ async function cards() {
           <img id="card_pic" src="https://media.4rgos.it/i/Argos/9520608_R_Z001A?w=750&h=440&qlt=70" width="100%" alt="phone-pic">
             </div><br><br>
             <div class="card_text">
-            <div class="cardsKeys" ><u>Brand</u>:<span class="cardsValues"> ${item.brand.toUpperCase()}</span></div>
-            <div class="cardsKeys" >Created At:<span class="cardsValues"> ${item.createdAt}</span></div>
-            <div class="cardsKeys" >Color:<span class="cardsValues"> ${item.color.toUpperCase()}</span></div>
-            <div class="cardsKeys" >Ram:<span class="cardsValues"> ${item.ram}</span></div>
-            <div class="cardsKeys" >is Available:<span class="cardsValues"> ${item.isAvailable.toUpperCase()}</span></div>
-            <div class="cardsKeys" >Price:<span class="cardsValues" > ${item.price} $</span></div></div><br>
+            <div ><div class="cardsKeys" >Brand:</div><span class="cardsValues"> ${item.brand.toUpperCase()}</span></div>
+            <div ><div class="cardsKeys" >Created At:</div><span class="cardsValues"> ${
+              item.createdAt
+            }</span></div>
+            <div ><div class="cardsKeys" >Color:</div><span class="cardsValues"> ${item.color.toUpperCase()}</span></div>
+            <div ><div class="cardsKeys" >Ram:</div><span class="cardsValues"> ${
+              item.ram
+            }</span></div>
+            <div ><div class="cardsKeys" >is Available:</div><span class="cardsValues"> ${item.isAvailable.toUpperCase()}</span></div>
+            <div ><div class="cardsKeys" >Price:</div><span class="cardsValues" > ${
+              item.price
+            } $</span></div></div><br>
             <button class="card_button" onclick="deleteCards()">DELETE</button><br>`);
         });
       });
