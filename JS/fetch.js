c5844548bf1785b3ff2c826a36ec69ecba0a3aa8
SolomonAvraham <Selmon_a@hotmail.com>
2022-07-09 16:21:02 +0300

    update

--- JS/fetch.js
+++ JS/fetch.js
@@ -13,25 +13,25 @@ async function userCardsSmallScreen() {
       .then((res) => {
         res.forEach((item) => {
           return (cardTextDisplay.innerHTML += ` 
-          <br><div  class="card_container" >
-          <img id="card_pic" src="https://media.4rgos.it/i/Argos/9520608_R_Z001A?w=750&h=440&qlt=70" width="100%" alt="phone-pic">
+          <div  class="card_container" >
+          <img class="card_pic" src="https://media.4rgos.it/i/Argos/9520608_R_Z001A?w=750&h=440&qlt=70" width="100%" alt="phone-pic">
             </div><br><br>
             <div class="card_text">
-            <div ><div class="cardsKeys" >Brand:<span class="cardsValues"> ${item.brand.toUpperCase()}</span></div></div><hr>
+            <div ><div class="cardsKeys" >Brand:<span class="cardsValues"> ${item.brand.toUpperCase()}</span></div></div><hr >
             <div ><div class="cardsKeys" >Created At:<span class="cardsValues"> ${
               item.createdAt
-            }</span></div></div><hr>
-            <div ><div class="cardsKeys" >Color:<span class="cardsValues"> ${item.color.toUpperCase()}</span></div></div><hr>
+            }</span></div></div><hr  >
+            <div ><div class="cardsKeys" >Color:<span class="cardsValues"> ${item.color.toUpperCase()}</span></div></div><hr >
             <div ><div class="cardsKeys" >Ram:<span class="cardsValues"> ${
               item.ram
-            }</span></div></div><hr>
-            <div ><div class="cardsKeys" >is Available:<span class="cardsValues"> ${item.isAvailable.toUpperCase()}</span></div></div><hr>
+            }</span></div></div><hr >
+            <div ><div class="cardsKeys" >is Available:<span class="cardsValues"> ${item.isAvailable.toUpperCase()}</span></div></div><hr  >
             <div ><div class="cardsKeys" >Price:<span class="cardsValues" > ${
               item.price
             } $</span></div></div></div><br>
             <button class="card_button" onclick="deleteCards(${
               item.id
-            })">DELETE</button><br> `);
+            })">DELETE</button>  `);
         });
       });
   } catch (error) {
@@ -105,8 +105,6 @@ async function addUserSmallScreen() {
     }).then((res) => res.json());
 
     console.log(response);
-
-
   } catch (error) {
   } finally {
   }
